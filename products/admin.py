647ad113b16db5cf8af3e08afe6cb9611b96a12b
Lenny Beto <lennybeto95@gmail.com>
2026-05-13 21:25:08 +0300

    feat: added admin deliverables for products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Product, Review, WishlistItem
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at")
+    prepopulated_fields = {"slug": ("name",)}
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "stock_quantity", "in_stock", "created_date")
+    list_filter = ("category", "created_date")
+    search_fields = ("name", "description")
+    readonly_fields = ("created_date", "updated_at")
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("product", "user", "rating", "created_at")
+
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "added_at")
